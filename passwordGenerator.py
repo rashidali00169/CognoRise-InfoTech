8c71dc3762af666a5b33831999c65831d1ee5376
rashidali00169 <mr.rashidali00169@gmail.com>
2024-06-06 01:49:15 -0700

    Add files via upload

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -7,7 +7,9 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+']
 
 print("Welcome to the Python Password Generator!")
@@ -17,7 +19,8 @@ totNumbers = int(input("How many numbers would you like in your password? "))
 
 
 
-myPassword = []   #for storing characters of the password
+myPassword = []                                  #for storing characters of the password
+
 for password in range(0, totLetters):
     myPassword  += random.choice(letters)       # getting random letters from the list
 
@@ -29,9 +32,11 @@ for password in range(0,totNumbers):
     myPassword += random.choice(numbers)        # getting random digits from the list
 
 
+random.shuffle(myPassword)                     #shuffle the list to mixup the password
 
-finalPassword = ""      #converting list into string of characters
+finalPassword = ""                             #converting list into string of characters
 
 for characters in myPassword:
     finalPassword += characters
+
 print(f"Here is your password: {finalPassword}")
