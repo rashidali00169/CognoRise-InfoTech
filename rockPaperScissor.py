a826353281ded8693875594593ff769d8eda093a
rashidali00169 <mr.rashidali00169@gmail.com>
2024-06-22 23:08:06 -0700

    Update rockPaperScissor.py

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -71,4 +71,29 @@ elif((myChoice == 0) and (computerChoice == 2 )) or ((myChoice == 2) and (comput
 else:
     print("Computer Win")
 
-print("Do You want to play again?")
+
+playAgain = input("Do you want to play again? Type 'Y' for YES and 'N' for NO: ")
+
+while (playAgain == 'y' or playAgain == 'Y'):
+
+    computerChoice = random.randint(0, 2)
+
+    myChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor: "))
+
+    print(f"Computer Choose {computerChoice}")
+    print(f"You choose {myChoice}")
+
+    if (myChoice >= 3 or myChoice < 0):
+        print("Give a valid number from [0, 1, 2]")
+    elif (myChoice == computerChoice):
+        print("Draw")
+    # checking all conditions for my winning
+    elif ((myChoice == 0) and (computerChoice == 2)) or ((myChoice == 2) and (computerChoice == 1)) or (
+            (myChoice == 1) and (computerChoice == 0)):  # Rock wins against scissor
+        print("You Win")
+
+    else:
+        print("Computer Win")
+
+    playAgain = input("Do you want to play again? Type 'Y' for YES and 'N' for NO: ")
+
