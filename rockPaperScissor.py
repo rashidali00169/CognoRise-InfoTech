dd2060ba4c99feb16c7b5ba35d22427696b7ec00
rashidali00169 <mr.rashidali00169@gmail.com>
2024-06-22 22:38:57 -0700

    Add files via upload

---
+++ rockPaperScissor.py
@@ -0,0 +1,67 @@
+
+#Project
+#Rock, Paper, Scisso Game
+
+import random
+
+rock = '''
+    _________
+___|     _____)
+        (_____)
+        (______)
+---      (____)
+  |_____(___)
+
+'''
+
+
+paper = '''
+    ____________
+___|     __________)
+        (____________)
+        (______________)
+---     (__________)
+  |_____(________)
+
+'''
+
+
+scissor = '''
+    _______
+___|  _____ )______
+         __________ )
+           __________
+        _____________ )
+        (_____)
+---|___(___)
+
+'''
+
+print(rock)
+print(paper)
+print(scissor)
+
+# 0 for Rock
+# 1 for paper
+# 2 for scissor
+
+
+computerChoice = random.randint(0,2)
+
+
+myChoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor: "))
+
+print(f"Computer Choose {computerChoice}")
+print(f"You choose {myChoice}")
+
+if (myChoice >= 3 or myChoice <0):
+    print("Give a valid number from [0, 1, 2]")
+elif(myChoice == computerChoice):
+    print("Draw")
+#checking all conditions for my winning
+elif((myChoice == 0) and (computerChoice == 2 )) or ((myChoice == 2) and (computerChoice == 1)) or ((myChoice == 1) and (computerChoice == 0)):  #Rock wins against scissor
+    print("You Win")
+
+else:
+    print("Computer Win")
+
